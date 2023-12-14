f677f6c9bb5d679d363bcee1e9f0087e30541214
Alongkorn <alongkorn.k@360innovative.com>
2023-12-14 09:49:51 +0700

    add new page

--- src/app/components/cards/productCards.tsx
+++ src/app/components/cards/productCards.tsx
@@ -1,59 +1,105 @@
 'use client'
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
-import Grid from '@mui/material/Unstable_Grid2';
+
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2";
+ 
+import { useRouter } from 'next/navigation'
 
 import { data } from "../../api/json/demo.ts";
 
-type Props = {}
-interface DataJson {
-    id: string
-    name: string
-  }
+export interface Root {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  brand: string;
+  category: string;
+  thumbnail: string;
+  images: Images;
+}
 
-export default function productCards({}: Props) {
-    
-    // const data = jsonFile.map((myData) => {
-    //     return myData
-    // })
+export interface Images {
+  "1": string;
+  "2": string;
+  "3": string;
+}
 
-    console.log(data);
+let products = [
+  {
+    id: 1,
+    title: "iPhone 9",
+    description: "An apple mobile which is nothing like apple",
+    price: 549,
+    discountPercentage: 12.96,
+    rating: 4.69,
+    stock: 94,
+    brand: "Apple",
+    category: "smartphones",
+    registration: new Date("2017-01-03"),
+    capacity: 7,
+    images: "ImagePlaceholder.png"
+  },
+  {
+    id: 2,
+    title: "iPhone 9",
+    description: "An apple mobile which is nothing like apple",
+    price: 549,
+    discountPercentage: 12.96,
+    rating: 4.69,
+    stock: 94,
+    brand: "Apple",
+    category: "smartphones",
+    registration: new Date("2017-01-03"),
+    capacity: 7,
+    images: "ImagePlaceholder2.png"
+  },
+];
 
-    return (
+export default function ProductCards({}: Root) {
+  const router = useRouter()
+
+  const myData = products.map((myData) => {
+    return myData.images;
+  });
+  console.log("data", myData);
+  const imgUrl = "/image/"
+
+  return (
     <>
-    <Grid container spacing={2}>
-        {/* <p>{data[0].id}</p> */}
-      
-        {data.map((data) => { 
-            
-              <Grid lg={4} key={data.id}>
+      <Grid container spacing={2}>
+        {products.map((data) => {
+          return (
+            <Grid lg={4} key={data.id}>
               <Card>
-              <CardActionArea>
-              <CardMedia
-                  component="img"
-                  height="140"
-                  image={data.image}
-                  alt="green iguana"
-              />
-              <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                  Lizard
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                  Lizards are a widespread group of squamate reptiles, with over 6,000
-                  species, ranging across all continents except Antarctica
-                  </Typography>
-              </CardContent>
-              </CardActionArea>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="140"
+                    image={imgUrl+data.images}
+                    alt={data.title}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {data.title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {data.description}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
               </Card>
-          </Grid>
+            </Grid>
+          );
         })}
-
-    </Grid>
-  </>
-  )
+      </Grid>
+    </>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
+"use client"
 import Image from "next/image";
 import Link from "next/link";
-
+import ProductCards from "./components/cards/productCards";
 export default function Home() {
   return (
     <>
-      Hello Word <br />
-      <Link href="/cart">go to cart</Link>
+      <ProductCards></ProductCards>
     </>
   );
 }
