dcdb3bf4bb5fb3ee204d90f08dede6743ff5cdd6
Hatsayakha <hatsaya13759@gmail.com>
2023-12-18 09:34:16 +0700

    Edit Banner

--- src/app/components/homepages/Carousel.tsx
+++ src/app/components/homepages/Carousel.tsx
@@ -28,12 +28,58 @@ const Carousel: React.FC<CarouselProps> = ({ images }) => {
   return (
     <div className="relative">
       <div className="flex items-center justify-between">
-        <button onClick={prevSlide} className="absolute left-0 top-1/2 transform -translate-y-1/2">
-          Previous
+      <button
+          onClick={prevSlide} 
+          type="button"
+          className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+          data-carousel-prev
+        >
+          <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            <svg
+              className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 6 10"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M5 1 1 5l4 4"
+              />
+            </svg>
+            <span className="sr-only">Previous</span>
+          </span>
         </button>
-        <button onClick={nextSlide} className="absolute right-0 top-1/2 transform -translate-y-1/2">
-          Next
+        <button
+          onClick={nextSlide}
+          type="button"
+          className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+          data-carousel-next
+        >
+          <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            <svg
+              className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 6 10"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m1 9 4-4-4-4"
+              />
+            </svg>
+            <span className="sr-only">Next</span>
+          </span>
         </button>
+        
+        
       </div>
       <img src={images[currentIndex]} alt={`Slide ${currentIndex + 1}`} className="w-full" />
     </div>
